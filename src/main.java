d49b3fdee2ce405c9f7d15149e527f04e7991744
Artur <artur.zulinski@gmail.com>
2021-02-04 00:57:40 +0100

    matrix x matrix done

--- src/main.java
+++ src/main.java
@@ -14,7 +14,8 @@ public class main {
             double val1=1, val2=1;
             double v1[] = new double[4];
             double v2[] = new double[4];
-            //double m1[][], m2[][];
+            double m1[][] = new double[4][4];
+            double m2[][] = new double[4][4];
 
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             choice1 = Integer.parseInt(br.readLine());
@@ -61,6 +62,7 @@ public class main {
                         for (int c = 0; c < column; c++) {
                             System.out.println(String.format("Enter first[%d][%d] integer", r, c));
                             first[r][c] = sc.nextInt();
+                            m1[r][c] = first[r][c];
                         }
                     }
                     for (int r = 0; r < row; r++) {
@@ -122,6 +124,7 @@ public class main {
                     for (int c = 0; c < column; c++) {
                         System.out.println(String.format("Enter second[%d][%d] integer", r, c));
                         second[r][c] = sc.nextInt();
+                        m2[r][c]=second[r][c];
                     }
                 }
                 for (int r = 0; r < row; r++) {
@@ -159,7 +162,7 @@ public class main {
                         System.out.println("Your result is: "+ Math.pow(val1,val2));
                         break;
                     case 6:
-                        //System.out.println("Your result is: "+ (val1+val2));
+                        System.out.println("Your result is: "+ Math.pow(val1,(1/val2)));
                         break;
                     default:
                         System.out.println("Wrong value");
@@ -169,16 +172,107 @@ public class main {
         if(choice1==1 && choice2==2){
             System.out.println("Your only option is to multiply the two.\nYour result is:");
             for (int i = 0; i < v2.length; i++) {
-                double result = val1 * v2[i];
-                System.out.printf(" "+ result);
+                if(v2[i]!=0){
+                System.out.printf(" "+ (val1*v2[i]));
+                }
             }
         }
 
         if(choice1==2 && choice2==1){
             System.out.println("Your only option is to multiply the two.\nYour result is:");
             for (int i = 0; i < v1.length; i++) {
-                double result = val2 * v1[i];
-                System.out.printf(" "+result);
+                if(v1[i]!=0){
+                System.out.printf(" "+(val2*v1[i]));
+                }
+            }
+        }
+
+        if(choice1==1 && choice2==3){
+            System.out.println("Your only option is to multiply the two.\nYour result is:");
+            for (int r = 0; r < m2.length; r++) {
+                for (int c = 0; c < m2[0].length; c++) {
+                    if(m2[r][c]!=0){
+                    System.out.printf(" "+ (val1 * m2[r][c])+"\t");
+                    }
+                }System.out.println();
+            }
+        }
+
+        if(choice1==3 && choice2==1){
+            System.out.println("Your only option is to multiply the two.\nYour result is:");
+            for (int r = 0; r < m1.length; r++) {
+                for (int c = 0; c < m1[0].length; c++) {
+                    if(m1[r][c]!=0){
+                        System.out.printf(" "+ (val2 * m1[r][c])+"\t");
+                    }
+                }System.out.println();
+            }
+        }
+
+        if(choice1==2 && choice2==2){
+            System.out.println("What do You want to do with inputted data:\n1.Add\n2.Subtraction");
+            BufferedReader br4 = new BufferedReader(new InputStreamReader(System.in));
+            int choice3 = Integer.parseInt(br4.readLine());
+
+            System.out.println("Your result: ");
+            switch (choice3){
+                case 1:
+                    for (int i = 0; i < v2.length; i++) {
+                        if(v2[i]!=0){
+                            System.out.printf(" "+ (v1[i]+v2[i]));
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < v2.length; i++) {
+                        if(v2[i]!=0){
+                            System.out.printf(" "+ (v1[i]-v2[i]));
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong value!!");
+                    break;
+            }
+        }
+
+        if(choice1==3 && choice2==3){
+            System.out.println("What do You want to do with inputted data:\n1.Add\n2.Subtraction\n3.Multiply");
+            BufferedReader br4 = new BufferedReader(new InputStreamReader(System.in));
+            int choice3 = Integer.parseInt(br4.readLine());
+
+            System.out.println("Your result:");
+            switch (choice3){
+                case 1:
+                    for(int i=0;i<m1.length;i++){
+                        for(int j=0;j<m1[0].length;j++){
+                            if(m1[i][j]!=0){
+                                System.out.printf(" "+ (m2[i][j] + m1[i][j])+"\t");
+                            }
+                        }System.out.println();
+                    }
+                    break;
+                case 2:
+                    for(int i=0;i<m1.length;i++){
+                        for(int j=0;j<m1[0].length;j++){
+                            if(m1[i][j]!=0){
+                                System.out.printf(" "+ (m2[i][j] - m1[i][j])+"\t");
+                            }
+                        }System.out.println();
+                    }
+                    break;
+                case 3:
+                    for(int i=0;i<m1.length;i++){
+                        for(int j=0;j<m1[0].length;j++){
+                            if(m1[i][j]!=0){
+                                System.out.printf(" "+ (m2[i][j] * m1[i][j])+"\t");
+                            }
+                        }System.out.println();
+                    }
+                    break;
+                default:
+                    System.out.println("Wrong value!!");
+                    break;
             }
         }
     }
