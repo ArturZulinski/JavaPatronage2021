5b564c3e477e8a4ea171849e479702706eed8e7a
Artur <artur.zulinski@gmail.com>
2021-02-04 21:20:57 +0100

    vector x matrix done

--- src/main.java
+++ src/main.java
@@ -19,15 +19,15 @@ public class main {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             choice1 = Integer.parseInt(br.readLine());
-
+//Chosing the first variant
             switch (choice1) {
-                case 1:
+                case 1:     //adding first number
                 System.out.println("What is Your first number:");
                 BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
                 val1 = Integer.parseInt(br1.readLine());
                 break;
 
-                case 2:
+                case 2:     //adding first vector
                 System.out.println("What is the size of Your vector (No bigger then 4):");
                 BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
                 int n = Integer.parseInt(br2.readLine());
@@ -45,7 +45,7 @@ public class main {
                 else System.out.printf("Wrong value!!");
                 break;
 
-                case 3:
+                case 3:     //adding first matrix
                     System.out.println("Please enter the rows in the matrix (No bigger then 4):");
                     Scanner sc = new Scanner(System.in);
                     int row = sc.nextInt();
@@ -273,6 +273,33 @@ public class main {
                 default:
                     System.out.println("Wrong value!!");
                     break;
+            }
+        }
+        if (choice1==2 && choice2==3){
+            double[] result = new double[m2.length];
+
+                    System.out.println("Your only option is to multiply the two.\nYour result is:");
+                    for (int i = 0; i < m2.length; i++) {
+                        for (int j = 0; j < m2[0].length; j++) {
+                            result[i] += m2[i][j] * v1[j];
+                        }
+                        if(result[i]!=0){
+                            System.out.println(" "+ result[i]);
+                        }
+            }
+        }
+
+        if(choice1==3 && choice2==2){
+            double[] result = new double[m1.length];
+
+            System.out.println("Your only option is to multiply the two.\nYour result is:");
+            for (int i = 0; i < m1.length; i++) {
+                for (int j = 0; j < m1[0].length; j++) {
+                    result[i] += m1[i][j] * v2[j];
+                }
+                if(result[i]!=0){
+                    System.out.println(" "+ result[i]);
+                }
             }
         }
     }
