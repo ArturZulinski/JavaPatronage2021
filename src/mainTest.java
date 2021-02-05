c41da4a9e18980d8bfde6134e8fce21f4c589c0b
Artur <artur.zulinski@gmail.com>
2021-02-05 23:23:38 +0100

    trying unit test

--- src/mainTest.java
+++ src/mainTest.java
@@ -1,5 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class mainTest {
 
+    @Test
+    public void first_test(){
+
+        //given
+        Integer choice1 = 1;
+        Double val1 = 3.0;
+        Integer choice2 = 1;
+        Double val2 = 2.0;
+        Integer choice3 = 1;
+        //when
+
+        //then
+        Assertions.assertEquals(5.0, 5);
+    }
 }
